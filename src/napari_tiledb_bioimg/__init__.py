13a58914cfd730f117d511a62d02dc022c14558c
George Sakkis <george.sakkis@tiledb.com>
2023-01-10 13:48:10 +0200

    initial commit

---
+++ src/napari_tiledb_bioimg/__init__.py
@@ -0,0 +1,13 @@
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+from ._reader import napari_get_reader
+from ._writer import write_multiple, write_single_image
+
+__all__ = (
+    "napari_get_reader",
+    "write_single_image",
+    "write_multiple",
+)
---
+++ src/napari_tiledb_bioimg/_tests/test_writer.py
@@ -0,0 +1,7 @@
+# from napari_tiledb_bioimg import write_single_image, write_multiple
+
+# add your tests here...
+
+
+def test_something():
+    pass
