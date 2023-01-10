075172e95756c751dba8a57e60cf749d5f34931a
George Sakkis <george.sakkis@tiledb.com>
2023-01-10 14:48:00 +0200

    Comment out writers for now

--- src/napari_tiledb_bioimg/__init__.py
+++ src/napari_tiledb_bioimg/__init__.py
@@ -4,10 +4,11 @@ except ImportError:
     __version__ = "unknown"
 
 from ._reader import napari_get_reader
-from ._writer import write_multiple, write_single_image
+
+# from ._writer import write_multiple, write_single_image
 
 __all__ = (
     "napari_get_reader",
-    "write_single_image",
-    "write_multiple",
+    # "write_single_image",
+    # "write_multiple",
 )
--- src/napari_tiledb_bioimg/_tests/test_writer.py
+++ src/napari_tiledb_bioimg/_tests/test_writer.py
@@ -1,7 +1,1 @@
 # from napari_tiledb_bioimg import write_single_image, write_multiple
-
-# add your tests here...
-
-
-def test_something():
-    pass
