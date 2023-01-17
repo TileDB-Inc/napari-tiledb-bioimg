d1b5c9a4d2a0a31dc93f3181946f8b90e7b1b4c4
George Sakkis <george.sakkis@tiledb.com>
2023-01-17 17:15:05 +0200

    Add reader tests

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,31 +1,58 @@
+import pathlib
+
 import numpy as np
+import pytest
+import tiledb
 
 from napari_tiledb_bioimg import napari_get_reader
 
+TEST_DIR = pathlib.Path(__file__).parent / "rand_uint16.tdb"
 
-# tmp_path is a pytest fixture
-def test_reader(tmp_path):
-    """An example of how you might test your plugin."""
 
-    # write some fake data using your supported file format
-    my_test_file = str(tmp_path / "myfile.npy")
-    original_data = np.random.rand(20, 20)
-    np.save(my_test_file, original_data)
+def test_get_reader_not_dir_fail(tmp_path):
+    """Not receiving a directory"""
+    f = tmp_path / "hello.txt"
+    assert not f.is_file()
+    f.write_text("")
+    assert f.is_file()
 
-    # try to read it back in
-    reader = napari_get_reader(my_test_file)
+    with pytest.warns(UserWarning, match="Not a directory"):
+        assert napari_get_reader(str(f)) is None
+
+
+def test_get_reader_invalid_dir_fail(tmp_path):
+    """Not receiving a proper TileDB group directory"""
+    with pytest.warns(UserWarning, match="Failed to open"):
+        assert napari_get_reader(str(tmp_path)) is None
+
+
+def test_get_reader_list_fail():
+    """Not receiving a single directory"""
+    with pytest.warns(UserWarning, match="Not a single path"):
+        assert napari_get_reader([str(TEST_DIR)]) is None
+
+
+def test_get_reader_succeed():
+    """Valid TileDB group dir"""
+    original_data = []
+    for level_dir in sorted(TEST_DIR.glob("l_*")):
+        with tiledb.open(str(level_dir)) as a:
+            original_data.append(a[:])
+
+    reader = napari_get_reader(str(TEST_DIR))
     assert callable(reader)
 
     # make sure we're delivering the right format
-    layer_data_list = reader(my_test_file)
-    assert isinstance(layer_data_list, list) and len(layer_data_list) > 0
+    layer_data_list = reader(str(TEST_DIR))
+    assert isinstance(layer_data_list, list) and len(layer_data_list) == 1
     layer_data_tuple = layer_data_list[0]
-    assert isinstance(layer_data_tuple, tuple) and len(layer_data_tuple) > 0
+    assert isinstance(layer_data_tuple, tuple) and len(layer_data_tuple) == 2
 
     # make sure it's the same as it started
-    np.testing.assert_allclose(original_data, layer_data_tuple[0])
-
-
-def test_get_reader_pass():
-    reader = napari_get_reader("fake.file")
-    assert reader is None
+    layers_data = layer_data_tuple[0]
+    assert isinstance(layers_data, list)
+    assert len(layers_data) == len(original_data)
+    for layer_data, orig_layer_data in zip(layers_data, original_data):
+        # original data are in CYX axes, layer data are YXC
+        transposed_layer_data = np.moveaxis(layer_data, 2, 0)
+        np.testing.assert_allclose(transposed_layer_data, orig_layer_data)
