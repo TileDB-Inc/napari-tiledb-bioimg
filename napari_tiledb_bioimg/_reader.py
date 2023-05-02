5250fa3b64b3b62e456bb8bcd10e5286c53d14a8
ktsitsi <kostas.t@tiledb.com>
2023-05-02 16:29:19 +0300

    Adding support for tiledb uris from cli

--- napari_tiledb_bioimg/_reader.py
+++ napari_tiledb_bioimg/_reader.py
@@ -1,28 +1,41 @@
 import os
 import warnings
-
-import tiledb
 from tiledb.bioimg.openslide import TileDBOpenSlide
 
 
 def napari_get_reader(path):
+
     if not isinstance(path, str):
         warnings.warn(f"Not a single path: {path}")
         return None
 
-    if tiledb.object_type(path) != "group":
-        warnings.warn(f"Not a tiledb group: {path}")
-        return None
+    config = None
+    if path.startswith('tiledb://'):
+        # This tiledb paths should be resolved with cloud creds found under $HOME./tiledb/cloud.json
+        # the current spec of napari_get_reader does not allow parameter passing
+        # Read through tiledb-cloud
+        try:
+            import tiledb.cloud
+            config  = tiledb.cloud.Config()
+        except:
+            raise ImportError("TileDB URIs require installation alongside tiledb-cloud")
+        
+    # Scope with ctx
+    with tiledb.scope_ctx(ctx_or_config=config):
 
-    try:
-        slide = TileDBOpenSlide(path)
-    except tiledb.TileDBError as ex:
-        warnings.warn(f"Failed to open {path}: {ex}")
-        return None
+        if tiledb.object_type(path) != "group":
+            warnings.warn(f"Not a tiledb group: {path}")
+            return None
 
-    def reader_function(_):
-        level_data = list(map(slide.read_level_dask, range(slide.level_count)))
-        name = os.path.basename(os.path.abspath(path))
-        return [(level_data, {"name": name})]
+        try:
+            slide = TileDBOpenSlide(path)
+        except tiledb.TileDBError as ex:
+            warnings.warn(f"Failed to open {path}: {ex}")
+            return None
 
-    return reader_function
+        def reader_function(_):
+            level_data = list(map(slide.read_level_dask, range(slide.level_count)))
+            name = os.path.basename(os.path.abspath(path))
+            return [(level_data, {"name": name})]
+
+        return reader_function
