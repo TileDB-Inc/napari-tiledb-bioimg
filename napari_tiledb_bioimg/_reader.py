9c6dbb1a8fc9b05d9cc489b1d0dbe3dfbdb1833c
Konstantinos Tsitsimpikos <ktsitsi@users.noreply.github.com>
2023-05-04 00:59:02 +0300

    Napari cli open tiledb and s3 uris (#3)
    * Fix windows test fails
    
    * linting etc.
    
    * Adding support for tiledb uris from cli
    
    * PR changes
    
    * Hack the fnmatch for opening tiledb uris
    
    * Add support for s3://
    
    * Fix import check for tiledb.cloud
    
    * Move import inside of get_reader function to fix tests

--- napari_tiledb_bioimg/_reader.py
+++ napari_tiledb_bioimg/_reader.py
@@ -1,28 +1,40 @@
 import os
 import warnings
 
-import tiledb
-from tiledb.bioimg.openslide import TileDBOpenSlide
-
-
 def napari_get_reader(path):
+    import tiledb
+    from tiledb.bioimg.openslide import TileDBOpenSlide
+
     if not isinstance(path, str):
         warnings.warn(f"Not a single path: {path}")
         return None
 
-    if tiledb.object_type(path) != "group":
-        warnings.warn(f"Not a tiledb group: {path}")
-        return None
+    config = None
+    if path.startswith('tiledb://'):
+        # This tiledb paths should be resolved with cloud creds found under $HOME./tiledb/cloud.json
+        # the current spec of napari_get_reader does not allow parameter passing
+        # Read through tiledb-cloud
+        try:
+          import tiledb.cloud
+          config = tiledb.cloud.Config()
+        except ImportError as exc:
+            raise ImportError("TileDB URIs require installation of tiledb-cloud") from exc
 
-    try:
-        slide = TileDBOpenSlide(path)
-    except tiledb.TileDBError as ex:
-        warnings.warn(f"Failed to open {path}: {ex}")
-        return None
+    # Scope with ctx
+    with tiledb.scope_ctx(ctx_or_config=config):
+        if tiledb.object_type(path) != "group":
+            warnings.warn(f"Not a tiledb group: {path}")
+            return None
 
-    def reader_function(_):
-        level_data = list(map(slide.read_level_dask, range(slide.level_count)))
-        name = os.path.basename(os.path.abspath(path))
-        return [(level_data, {"name": name})]
+        try:
+            slide = TileDBOpenSlide(path)
+        except tiledb.TileDBError as ex:
+            warnings.warn(f"napari-tiledb plugin failed to open {path}: {ex}")
+            return None
 
-    return reader_function
+        def reader_function(_):
+            level_data = list(map(slide.read_level_dask, range(slide.level_count)))
+            name = os.path.basename(os.path.abspath(path))
+            return [(level_data, {"name": name})]
+
+        return reader_function
