74388f54d30b4083a9c17479b3362200ddc19609
Konstantinos Tsitsimpikos <ktsitsi@users.noreply.github.com>
2023-05-11 18:15:01 +0300

    Adding a scale bar and unit information from metadata (#9)
    * Adding a scale bar and unit information from metadata
    
    * Fixing LayerData length in tests
    
    * Adding try-except block for scale bar
    
    * Fix reading of images without scale metadata (ihnorton)
    
    ---------
    
    Co-authored-by: Isaiah Norton <isaiah.norton@gmail.com>

--- napari_tiledb_bioimg/_reader.py
+++ napari_tiledb_bioimg/_reader.py
@@ -1,5 +1,19 @@
 import os
 import warnings
+import json
+
+from typing import Sequence, Mapping, Any
+
+
+def _get_meta(name: str, level_metadata: Sequence[Any]) -> Mapping[str, Any]:
+    try:
+        # TODO: clean this up
+        meta: Mapping[str, Any] = {"name": name, "metadata": dict(json.loads(level_metadata[0]['json_write_kwargs']))}
+    except KeyError as exc:
+        warnings.warn(f"[TileDB:Napari:BioImg] napari-tiledb plugin did not find expected metadata: {exc}")
+        return dict()
+    return meta
+
 
 def napari_get_reader(path):
     import tiledb
@@ -18,7 +32,7 @@ def napari_get_reader(path):
           import tiledb.cloud
           config = tiledb.cloud.Config()
         except ImportError as exc:
-            raise ImportError("TileDB URIs require installation of tiledb-cloud") from exc
+            raise ImportError("[TileDB:Napari:BioImg] TileDB URIs require installation of tiledb-cloud") from exc
 
     # Scope with ctx
     with tiledb.scope_ctx(ctx_or_config=config):
@@ -29,12 +43,14 @@ def napari_get_reader(path):
         try:
             slide = TileDBOpenSlide(path)
         except tiledb.TileDBError as ex:
-            warnings.warn(f"napari-tiledb plugin failed to open {path}: {ex}")
+            warnings.warn(f"[TileDB:Napari:BioImg] napari-tiledb plugin failed to open {path}: {ex}")
             return None
 
         def reader_function(_):
             level_data = list(map(slide.read_level_dask, range(slide.level_count)))
+            level_metadata = list(map(slide.level_properties, range(slide.level_count)))
             name = os.path.basename(os.path.abspath(path))
-            return [(level_data, {"name": name})]
+            meta = _get_meta(name, level_metadata)
+            return [(level_data, meta, "image")]
 
         return reader_function
