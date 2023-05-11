57b552d4566bd8ed8cdba68a6bed00af7dd48a4d
Konstantinos Tsitsimpikos <ktsitsi@users.noreply.github.com>
2023-05-11 16:16:01 +0300

    Add sample data button in TileDB Widget (#7)
    * Adding sample data
    
    * Adding a sample button and a sample images in tiledb widget

--- napari_tiledb_bioimg/_widget.py
+++ napari_tiledb_bioimg/_widget.py
@@ -15,15 +15,29 @@ class TileDBWidget(QWidget):
         layout = QtWidgets.QFormLayout()
         fields_grow = QtWidgets.QFormLayout.ExpandingFieldsGrow
         self.setLayout(layout)
+
+        # Path placeholder
         layout.setFieldGrowthPolicy(fields_grow)
         self.path = os.path.dirname(os.path.realpath(__file__))
         self.folderEdit = QtWidgets.QLineEdit(self.path)
         layout.insertRow(0, 'Path:', self.folderEdit)
+
+        # Open Button
         self.openbtn = QtWidgets.QPushButton('Open')
         self.openbtn.clicked.connect(self._open_tdb)
         layout.insertRow(1, self.openbtn)
+
+        # Sample button
+        self.sample_btn = QtWidgets.QPushButton('Load Sample Dataset')
+        self.sample_btn.clicked.connect(self._sample_tdb)
+        layout.insertRow(2, self.sample_btn)
 
     def _open_tdb(self):
         tdb_path = self.folderEdit.text()
         if tdb_path:
             self.viewer.open(tdb_path, plugin='napari-tiledb-bioimg')
+
+    def _sample_tdb(self):
+        self.viewer.open(os.path.join(self.path, 'samples/CMU-1-Small-Region-rgb.ome.tiff.tdb'),
+                         plugin='napari-tiledb-bioimg'
+                         )
