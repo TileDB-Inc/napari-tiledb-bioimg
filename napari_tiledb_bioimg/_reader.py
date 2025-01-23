7a0736bab988c891c21ee164a89f9f7147bb1d1f
ktsitsi <kostas.t@tiledb.com>
2025-01-23 10:20:42 -0500

    Updating the valid path logic and sample data

--- napari_tiledb_bioimg/_reader.py
+++ napari_tiledb_bioimg/_reader.py
@@ -23,38 +23,47 @@ def napari_get_reader(path):
         warnings.warn(f"Not a single path: {path}")
         return None
 
-    if not os.path.exists(path):
-        warnings.warn(f"Not a valid path {path}")
-        raise ValueError('[TileDB::Napari] - Error: The path given is not valid or does not exist')
-    
     config = None
+    valid = False
+    if os.path.exists(path):
+        valid=True
+    else: 
+        warnings.warn(f"Not a local valid path {path}")
+    
     if path.startswith('tiledb://'):
         # This tiledb paths should be resolved with cloud creds found under $HOME./tiledb/cloud.json
         # the current spec of napari_get_reader does not allow parameter passing
         # Read through tiledb-cloud
+        valid=True
         try:
           import tiledb.cloud
           config = tiledb.cloud.Config()
         except ImportError as exc:
             raise ImportError("[TileDB:Napari:BioImg] TileDB URIs require installation of tiledb-cloud") from exc
+    
+    if path.startswith('s3://'):
+        valid=True
 
-    # Scope with ctx
-    with tiledb.scope_ctx(ctx_or_config=config):
-        if tiledb.object_type(path) != "group":
-            warnings.warn(f"Not a tiledb group: {path}")
-            return None
+    if valid:
+        # Scope with ctx
+        with tiledb.scope_ctx(ctx_or_config=config):
+            if tiledb.object_type(path) != "group":
+                warnings.warn(f"Not a tiledb group: {path}")
+                return None
 
-        try:
-            slide = TileDBOpenSlide(path)
-        except tiledb.TileDBError as ex:
-            warnings.warn(f"[TileDB:Napari:BioImg] napari-tiledb plugin failed to open {path}: {ex}")
-            return None
+            try:
+                slide = TileDBOpenSlide(path)
+            except tiledb.TileDBError as ex:
+                warnings.warn(f"[TileDB:Napari:BioImg] napari-tiledb plugin failed to open {path}: {ex}")
+                return None
 
-        def reader_function(_):
-            level_data = list(map(slide.read_level_dask, range(slide.level_count)))
-            level_metadata = list(map(slide.level_properties, range(slide.level_count)))
-            name = os.path.basename(os.path.abspath(path))
-            meta = _get_meta(name, level_metadata)
-            return [(level_data, meta, "image")]
+            def reader_function(_):
+                level_data = list(map(slide.read_level_dask, range(slide.level_count)))
+                level_metadata = list(map(slide.level_properties, range(slide.level_count)))
+                name = os.path.basename(os.path.abspath(path))
+                meta = _get_meta(name, level_metadata)
+                return [(level_data, meta, "image")]
 
-        return reader_function
+            return reader_function
+    else:
+        raise ValueError('[TileDB::Napari] - Error: The path given is not valid or does not exist')
